9e70ca078f4087be72aa6f708d56cb0707a9831c
Rodrigo Silva <rodrigo.parke.silva@gmail.com>
2018-07-30 15:56:21 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Gerenciador de notícias com JavaScript
-
-[![DOI](https://zenodo.org/badge/141930756.svg)](https://zenodo.org/badge/latestdoi/141930756)
+# gerenciador de notícias com JavaScript
 
 Esse é um software de gerenciamento de notícias, fornecendo as seguintes funcionalidades:
 
--- main.js
+++ main.js
@@ -1,52 +1,82 @@
 var noticias = [];
 
+
 function atualizarLista(noticia) {
-    var lista = document.getElementById('noticias-recentes-list');
-    var li = document.createElement('li');
-    li.setAttribute('id', 'noticia-' + noticia.id);
-    li.setAttribute('class', 'noticia');
-    li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
-        + noticia.titulo
-        + '</p>'
-        + '<p class="conteudo">'
-        + noticia.conteudo
+    var lista = $('#noticias-recentes-list');
+    var li = $('<li id="noticia-' + noticia.id + '">');
+    li.addClass('noticia');
+    var p_titulo = $('<p>');
+    p_titulo.addClass('titulo');
+    p_titulo.attr('onclick', 'mostrarNoticia(' + noticia.id + ')');
+    p_titulo.html(noticia.titulo);
+    var p_conteudo = $('<p>');
+    p_conteudo.addClass('conteudo');
+    p_conteudo.html(noticia.conteudo
         + '<br>'
-        + '<span>------------------</span>'
         + '<br>'
-        + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
-        + '</p>';
-    lista.appendChild(li);
+        + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>' + '<br>'
+        + '<button onclick="mostrarAutor(' + noticia.id + ')">Mostrar dados do autor</button>' + '<br>'
+        + '<button onclick="ocultarAutor(' + noticia.id + ')">Ocultar dados do autor</button>');
+    var p_autor = $('<p>');
+    p_autor.addClass('autor');
+    p_autor.html(
+        noticia.nome
+        + '<br>' +
+        noticia.email
+        + '<br>' +
+        noticia.data_publicacao);
+    li.append(p_titulo, p_conteudo, p_autor);
+    p_autor.hide();
+    p_conteudo.hide();
+    if (noticia.data_publicacao != undefined) {
+        if (noticia.data_publicacao > noticia.data_atual || noticia.data_publicacao < noticia.data_atual) {
+            alert("Sua notícia foi salva, mas ainda não foi publicada! Aguarde.");
+        }
+        else {
+            lista.append(li);
+            alert("Notícia publicada!");
+        }
+    }
 }
 
 function salvar(form) {
-    var titulo = document.getElementById('frm-titulo').value;
-    var conteudo = document.getElementById('frm-conteudo').value;
+    var titulo = $('#frm-titulo').val();
+    var conteudo = $('#frm-conteudo').val();
+    var nome = $('#frm-nome').val();
+    var email = $('#frm-email').val();
+    var data_publicacao = new Date($('#frm-data').val()).toDate('dd/mm/yyyy hh:ii');
+    var data_atual = new Date().toDate('dd/mm/yyyy hh:ii');
     var noticia = {
         id: noticias.length,
         titulo: titulo,
-        conteudo: conteudo
+        conteudo: conteudo,
+        nome: nome,
+        email: email,
+        data_publicacao: data_publicacao,
+        data_atual: data_atual
     };
-    noticias.push(noticia);
-    atualizarLista(noticia);
-    form.reset();
+    noticias.unshift(noticia);
+    var list = document.getElementById('noticias-recentes-list');
+    list.innerHTML = '';
+    for (var i = 0; i < noticias.length; i++) {
+        atualizarLista(noticias[i])
+    };
+    //form.reset();
 }
 
 function mostrarNoticia(id) {
-    var li = document.getElementById('noticia-' + id);
-    for (var i = 0; i < li.childNodes.length; i++) {
-        var node = li.childNodes[i];
-        if (node.getAttribute('class') == 'conteudo') {
-            node.setAttribute('style', 'display:inline');
-        }
-    }
+    $('.conteudo', '#noticia-' + id).show();
 }
 
 function ocultarNoticia(id) {
-    var li = document.getElementById('noticia-' + id);
-    for (var i = 0; i < li.childNodes.length; i++) {
-        var node = li.childNodes[i];
-        if (node.getAttribute('class') == 'conteudo') {
-            node.setAttribute('style', 'display:none');
-        }
-    }
+    $('.conteudo', '#noticia-' + id).hide();
 }
+
+function ocultarAutor(id) {
+    $('.autor', '#noticia-' + id).hide();
+}
+
+function mostrarAutor(id) {
+    $('.autor', '#noticia-' + id).show();
+}
+
